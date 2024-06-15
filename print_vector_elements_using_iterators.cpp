b7a47b2fdd881dee4f40411a1479a4f2aa70f3dd
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-15 14:07:37 +0300

    Add files via upload
    print_vector_elements_using_iterators

---
+++ print_vector_elements_using_iterators.cpp
@@ -0,0 +1,24 @@
+﻿/*
+     YILDIZ ★
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+   /* vector<int> v = {10,20,30,40};
+    for (vector<int>::const_reverse_iterator it=v.crbegin();it!=v.crend(); it++)
+    {
+        cout << *it << "\n";
+    } */
+   
+   // auto x = "xxx";
+    vector<int> v = { 10,20,30,40 };
+    auto it = v.begin();
+    for (it; it!=v.end(); it++)
+    {
+        cout << *it << "\n";
+    }
+
+}
+
